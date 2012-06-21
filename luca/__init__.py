3f7db7ccac1698d69114fb465407d64b083da6b1
Brandon Rhodes <brandon@rhodesmill.org>
2012-06-20 22:40:59 -0400

    Started adding a few details to the docstring

--- luca/__init__.py
+++ luca/__init__.py
@@ -1,3 +1,12 @@
 """Bookkeeping for Python: accounting, OFX, taxes
 
+The **luca** command lets you manage your finances
+using the same tools you use to manage the rest of your life:
+plain-text files, version control, and the command line.
+
+The name is pronounced *lew-cha* because it is Italian.
+The tool is named for Luca Pacioli,
+the Franciscan friar credited with the invention
+of double-entry bookkeeping.
+
 """
