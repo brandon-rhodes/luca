491997a3b3de82570e53d559050385f1fce5b6d3
Brandon Rhodes <brandon@rhodesmill.org>
2013-04-12 10:54:45 -0400

    Reboot of the Form concept, this time using TDD

---
+++ luca/forms/form.py
@@ -0,0 +1,4 @@
+'''The Form class and its supporting materials.'''
+
+class Form(object):
+    pass
---
+++ luca/tests/test_form.py
@@ -0,0 +1,43 @@
+'''Tests of the important Form class.'''
+
+from unittest import TestCase
+from luca.forms.form import Form
+
+
+class FormTests(TestCase):
+
+    def test_instantiation(self):
+        Form()
+
+
+json1 = '''{
+ "inputs": {
+  "form": "rental_income",
+  "ssn": "123-45-6789",
+  "name": "Lynn Smith",
+  "A": {
+   "address": "123 Main St, Ohio",
+   "rents": "900.00",
+   "expenses": "100.00",
+  },
+  "B": {
+   "address": "456 Elm St, Georgia",
+   "rents": "800.00",
+   "expenses": "230.00",
+  }
+ },
+ "outputs": {
+  "A": {
+   "profit": "800.00",
+   "tax": "80.00"
+  },
+  "B": {
+   "profit": "570.00"
+   "tax": "57.00"
+  },
+  "total_rents": "1700.00",
+  "total_expenses": "330.00",
+  "total_tax": "137.00"
+ }
+}
+'''
