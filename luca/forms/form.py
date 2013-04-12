d55a2aa4332fc163d775f8dad6d71906128cc790
Brandon Rhodes <brandon@rhodesmill.org>
2013-04-12 11:46:02 -0400

    Refactored Form to not use O(n) list searches

--- luca/forms/form.py
+++ luca/forms/form.py
@@ -4,21 +4,28 @@ class Form(object):
     '''A class whose instances remember the order in which attrs are set.'''
 
     def __init__(self):
-        self._inputs = []
+        self._clear()
         self._outputs = []
-        self._names = self._inputs
+        self._outputset = set()
+        self._mode = 'input'
 
     def _clear(self):
-        del self._inputs[:]
+        self._inputs = []
+        self._inputset = set()
 
     def _switch_from_input_to_output(self):
-        self._names = self._outputs
+        self._mode = 'output'
 
     def __setattr__(self, name, value):
         if not name.startswith('_'):
-            if name in self._inputs:
+            if name in self._inputset:
                 raise TypeError('an input attribute like %s can only be'
                                 ' set once' % name)
-            if name not in self._names:
-                self._names.append(name)
+            if self._mode == 'input':
+                self._inputs.append(name)
+                self._inputset.add(name)
+            else:
+                if name not in self._outputset:
+                    self._outputs.append(name)
+                    self._outputset.add(name)
         super(Form, self).__setattr__(name, value)
