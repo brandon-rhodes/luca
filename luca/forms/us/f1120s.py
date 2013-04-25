12e0470069bc68d1fc604e2e7f4a11b665eee780
Brandon Rhodes <brandon@rhodesmill.org>
2013-04-25 15:05:22 -0400

    Teach form 1120S to fill out its first page

--- luca/forms/us/f1120s.py
+++ luca/forms/us/f1120s.py
@@ -1,4 +1,5 @@
 from decimal import Decimal
+from luca.forms.formlib import Form
 from luca.kit import cents, validate, zero, zzstr
 
 
@@ -11,12 +12,12 @@ def defaults(form):
     f.street = ''
     f.city_state_zip = ''
 
-    f.A = f.B = f.E = ''
-    f.C = False
-    f.F = zero
-    f.G = False
-    f.H = 1
-    f.I = 1
+    f.lineA = f.lineB = f.lineE = ''
+    f.lineC = False
+    f.lineF = zero
+    f.lineG = False
+    f.lineH = 1
+    f.lineI = 1
 
     f.line1a = f.line1b = f.line2 = f.line4 = f.line5 = zero
     for n in range(7, 20):
@@ -68,12 +69,6 @@ def defaults(form):
         f.K['line15', chr(i)] = zero
     for i in range(ord('a'), ord('e') + 1):
         f.K['line16', chr(i)] = zero
-    f.K.line17a = f.K.line17b = f.K.line17c = zero
-    f.K.line18 = (
-        sum(f.K['line', i] for i in range(1, 10 + 1))
-        - f.K.line11 - f.K.line12a - f.K.line12b - f.K.line12c
-        - f.K.line12d - f.line14l
-        )
 
     # TODO: Schedule L
     # TODO: Schedule M-1
@@ -90,6 +85,7 @@ def defaults(form):
     f.M2.line7b = zero
 
     f.M2.line1c = zero
+    f.M2.line7c = zero
 
 
 def compute(form):
@@ -100,7 +96,8 @@ def compute(form):
     f.line3 = f.line1c - f.line2
     f.line6 = f.line3 + f.line4 + f.line5
 
-    f.line21 = sum(f['line', n] for n in range(7, 20))
+    f.line20 = sum(f['line', n] for n in range(7, 20))
+    f.line21 = f.line6 - f.line20
 
     f.line22c = f.line22a + f.line22b
     f.line23d = f.line23a + f.line23b + f.line23c
@@ -115,14 +112,75 @@ def compute(form):
         f.line27 = -owed - f.line27_credited
 
     f.K.line3c = f.K.line3a - f.K.line3b
+    f.K.line17a = f.K.line17b = f.K.line17c = zero
+    f.K.line18 = (
+        f.K.line1 + f.K.line2 + f.K.line3c + f.K.line4
+        + f.K.line5a + f.K.line6 + f.K.line7 + f.K.line8a
+        + f.K.line9 + f.K.line10
+        - f.K.line11 - f.K.line12a - f.K.line12b - f.K.line12c
+        - f.K.line12d - f.K.line14l
+        )
 
     f.M2.line2a = abs(f.line21) if f.line21 > 0 else zero
     f.M2.line4a = abs(f.line21) if f.line21 < 0 else zero
     f.M2.line6a = sum(f.M2['line', n, 'a'] for n in range(1, 5+1))
-    f.M2.line8a = f.line6a - f.line7a
+    f.M2.line8a = f.M2.line6a - f.M2.line7a
 
     f.M2.line6b = f.M2.line1b + f.M2.line3b + f.M2.line5b
-    f.M2.line8b = f.line6b - f.line7b
+    f.M2.line8b = f.M2.line6b - f.M2.line7b
 
     f.M2.line6c = f.M2.line1c
-    f.M2.line8c = f.line6c - f.line7c
+    f.M2.line8c = f.M2.line6c - f.M2.line7c
+
+
+def fill_out(form, pdf):
+    f = form
+    pdf.load('us.f1120s--{}.pdf'.format(f.year))
+
+    def field(n):
+        return 'p{}-t{}[0]'.format(page, n)
+
+    def pair(n, value):
+        pdf[field(n)], pdf[field(n + 1)] = zzstr(value)
+
+    page = 1
+
+    # TODO: dates for non-calendar tax year
+
+    pdf['p1-t4['] = f.name
+    pdf['p1-t5['] = f.street
+    pdf['p1-t6['] = f.city_state_zip
+
+    pdf['p1-t7['] = f.lineA
+    pdf['p1-t8['] = f.lineB
+    pdf['c1_01_0_[0]'] = 'Yes' if f.lineC else 'Off'
+    pdf['p1-t9['] = f.ein
+    pdf['p1-t11['] = f.lineE
+    pair(12, f.lineF)
+
+    pdf['c1_02_0_[0]'] = 'Yes' if f.lineG else 'Off'
+    pdf['c1_02_0_[1]'] = 'Off' if f.lineG else 'No'
+    for i in range(1, 5+1):
+        pdf['c1_0{}_0_[0]'.format(i + 2)] = ('Yes' if f.lineH == i else 'Off')
+    pdf['p1-t14['] = str(f.lineI)
+
+    pair(15, f.line1a)
+    pair(17, f.line1b)
+    pair(19, f.line1c)
+    for i in range(2, 21+1):
+        pair(2 + 17, f['line', i])
+    pair(61, f.line22a)
+    pair(63, f.line22b)
+    pair(65, f.line22c)
+    pair(67, f.line23a)
+    pair(69, f.line23b)
+    pair(71, f.line23c)
+    pair(73, f.line23d)
+    pair(75, f.line24)
+    pair(77, f.line25)
+    pair(79, f.line26)
+    pair(81, f.line27_credited)
+    pair(83, f.line27)
+    pdf['p1_t85'] = f.signer_title
+    pdf['c1_9_0_[0]'] = 'Yes' if f.discuss else 'Off'
+    pdf['c1_9_0_[1]'] = 'Off' if f.discuss else 'No'
