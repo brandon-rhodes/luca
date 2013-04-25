3696471ed3c44954c63b038f9084bcec34c9a6ad
Brandon Rhodes <brandon@rhodesmill.org>
2013-04-25 11:08:40 -0400

    Roughed out Schedule B and Schedule K of 1120S

--- luca/forms/us/f1120s.py
+++ luca/forms/us/f1120s.py
@@ -28,6 +28,53 @@ def defaults(form):
     f.signer_title = ''
     f.discuss = False
 
+    f.B = Form()
+    f.B.line1 = 'a'
+    f.B.line2a = ''
+    f.B.line2b = ''
+    f.B.line3 = False
+    # TODO: tables beneath line 4a and line4b
+    f.B.line4a = []
+    f.B.line4b = []
+    f.B.line5ai = 0
+    f.B.line5aii = 0
+    f.B.line5bi = 0
+    f.B.line5bii = 0
+    f.B.line6 = False
+    f.B.line7 = False
+    f.B.line8 = zero
+    f.B.line9 = zero
+    f.B.line10 = True
+    f.B.line11 = zero
+    f.B.line12 = False
+    f.B.line13a = False
+    f.B.line13b = False
+
+    f.K = Form()
+    f.K.line1 = f.K.line2 = zero
+    f.K.line3a = f.K.line3b = zero
+    f.K.line4 = f.K.line5a = f.K.line5c = f.K.line6 = f.K.line7 = zero
+    f.K.line8a = f.K.line8b = f.K.line8c = f.K.line9 = f.K.line10 = zero
+    f.K.line11 = f.K.line12a = f.K.line12b = f.K.line12c = f.K.line12d = zero
+    f.K.line12c_type = ''
+    f.K.line13a = f.K.line13b = f.K.line13c = f.K.line13d = zero
+    f.K.line13e = f.K.line13f = f.K.line13g = zero
+    f.K.line13d_type = ''
+    f.K.line13e_type = ''
+    f.K.line13g_type = ''
+    for i in range(ord('a'), ord('n') + 1):
+        setattr(f.K, 'line14' + chr(i), zero)
+    for i in range(ord('a'), ord('f') + 1):
+        setattr(f.K, 'line15' + chr(i), zero)
+    for i in range(ord('a'), ord('e') + 1):
+        setattr(f.K, 'line16' + chr(i), zero)
+    f.K.line17a = f.K.line17b = f.K.line17c = zero
+    f.K.line18 = (
+        sum(getattr(f.K, 'line{}'.format(i)) for i in range(1, 10 + 1))
+        - f.K.line11 - f.K.line12a - f.K.line12b - f.K.line12c
+        - f.K.line12d - f.line14l
+        )
+
 
 def compute(form):
     f = form
@@ -50,3 +97,6 @@ def compute(form):
         f.line25 = zero
         f.line26 = -owed
         f.line27 = -owed - f.line27_credited
+
+    f.K.line3c = f.K.line3a - f.K.line3b
+    
