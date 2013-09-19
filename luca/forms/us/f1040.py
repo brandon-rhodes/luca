90f267e0d6ccbff3b3a32bb34107393fe4b01222
Brandon Rhodes <brandon@rhodesmill.org>
2013-09-18 23:43:14 -0400

    Form 1040 can now compute a final tally either way
    For now the tax table is inline with the source code as a CSV file;
    later, it should probably be downloaded at runtime from S3 like the tax
    forms themselves are.

--- luca/forms/us/f1040.py
+++ luca/forms/us/f1040.py
@@ -1,16 +1,21 @@
-from luca.kit import dsum, zero, zzstr
+from luca.kit import Decimal, dsum, zero, zzstr
 
 title = u'Form 1040: U.S. Individual Income Tax Return'
 
 filing_statuses = 'S MJ MS HoH QW'.split()
 
-def lines(string):
-    return ['line' + suffix for suffix in string.split()]
+def lines(seq):
+    if isinstance(seq, str):
+        seq = seq.split()
+    return ['line{}'.format(suffix) for suffix in seq]
 
 income_lines = lines('7 8a 8b 9a 9b 10 11 12 13 14 15a 15b '
                      '16a 16b 17 18 19 20a 20b 21')
 income_tally = lines('7 8a 9a 10 11 12 13 14 15b 16b 17 18 19 20b 21')
-adjustment_lines = lines('23 24 25 26 27 28 29 30 31a 32 33 34 35')
+adjustments = lines('23 24 25 26 27 28 29 30 31a 32 33 34 35')
+credits = lines(range(47, 54))
+other_taxes = lines('56 57 58 59a 59b 60')
+payments = lines(range(62, 64) + ['64a', '64b'] + range(65, 72))
 
 def defaults(form):
     f = form
@@ -34,21 +39,27 @@ def defaults(form):
     f.line6a = False
     f.line6b = False
 
-    for line in income_lines + adjustment_lines:
+    for line in income_lines + adjustments + credits + other_taxes + payments:
         setattr(f, line, zero)
 
     f.line21_text = u''
     f.line32b = u''
 
+    f.line40 = zero
+    f.line45 = zero
+
 
 def check(form, forms, eq):
     nothing = [None]
+
+    f1040sa = forms.get('us.f1040sa', nothing)[0]
+    if f1040sa:
+        eq('line40', f1040sa.line29)
 
     f1040sb = forms.get('us.f1040sb', nothing)[0]
     if f1040sb:
         eq('line8a', f1040sb.line4)
         eq('line9a', f1040sb.line6)
-        # TODO: further instructions about if it comes out negative
 
     f1040sc = forms.get('us.f1040sc', nothing)[0]
     if f1040sc:
@@ -74,8 +85,24 @@ def compute(form):
     # TODO: dependents
     f.line6d = f.line6ab
     f.line22 = dsum(getattr(f, line) for line in income_tally)
-    f.line36 = dsum(getattr(f, line) for line in adjustment_lines)
+    f.line36 = dsum(getattr(f, line) for line in adjustments)
     f.line37 = f.line22 - f.line35
+    f.line38 = f.line37
+    # TODO: standard deduction
+    f.line41 = f.line38 - f.line40
+    f.line42 = f.line6d * Decimal('3800.00')
+    f.line43 = max(f.line41 - f.line42, zero)
+    f.line44 = tax_from_tax_table(f.line43, f.filing_status)
+    f.line46 = f.line44 + f.line45
+    f.line54 = dsum(getattr(f, line) for line in credits)
+    f.line55 = f.line46 - f.line54
+    f.line61 = f.line55 + dsum(getattr(f, line) for line in other_taxes)
+    f.line72 = dsum(getattr(f, line) for line in payments) - f.line64b
+    if f.line72 > f.line61:
+        f.line73 = f.line72 - f.line61
+        # TODO: asking for refund etc
+    elif f.line61 > f.line72:
+        f.line76 = f.line61 - f.line72
 
 
 def fill_out(form, pdf):
@@ -116,12 +143,61 @@ def fill_out(form, pdf):
     pdf[45] = str(f.line6d)
 
     n = 46
-    for line in income_lines + lines('22') + adjustment_lines + lines('36 37'):
+    for line in income_lines + lines('22') + adjustments + lines('36 37'):
         pdf[n], pdf[n+1] = zzstr(getattr(f, line))
         if n == 82:
-            pdf[84] = f.line21_text
             n = 85
         elif n == 103:
             n = 108
         else:
             n += 2
+
+    pdf.pattern = '{}'
+
+    pdf['Page1[0].p1-t84[0]'] = f.line21_text
+
+    pdf.pattern = 'p2-t{}[0]'
+
+    n = 1
+    for line in (lines([38] + range(40, 47)) + credits + lines('54 55')
+                 + other_taxes + lines('61') + payments + lines('72 73 74a')
+                 + lines(range(75, 78))):
+        pdf[n], pdf[n+1] = zzstr(getattr(f, line, zero)) # todo: remove default
+        if n == 1:
+            n = 4
+        elif n == 20:
+            n = 24
+        elif n == 32:
+            n = 37
+        elif n == 45 and pdf.pattern == 'p2-t{}[0]':
+            pdf.pattern = 'p2-t{}[1]'
+        elif n == 45 and pdf.pattern == 'p2-t{}[1]':
+            pdf.pattern = 'p2-t{}[2]'
+        elif n == 45 and pdf.pattern == 'p2-t{}[2]':
+            pdf.pattern = 'p2-t{}[0]'
+            n = 47
+        elif n == 49:
+            n = 53
+        elif n == 69 and pdf.pattern == 'p2-t{}[0]':
+            pdf.pattern = 'p2-t{}[1]'
+        elif n == 69 and pdf.pattern == 'p2-t{}[1]':
+            pdf.pattern = 'p2-t{}[0]'
+            n = 71
+        elif n == 77:
+            n = 105
+        else:
+            n += 2
+
+
+def tax_from_tax_table(taxable_income, filing_status):
+    import csv
+    import os
+    here = os.path.dirname(__file__)
+    with open(os.path.join(here, 'f1040-tax-table.csv')) as f:
+        for row in csv.DictReader(f):
+            minimum = Decimal(row['At Least'])
+            maximum = Decimal(row['But Less Than'])
+            if minimum <= taxable_income < maximum:
+                return Decimal(row[filing_status] + '.00')
+        else:
+            return Decimal('1.23')
