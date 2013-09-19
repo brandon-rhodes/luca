35ce018c77934a7e464adcd2a18dd09bc1a8a052
Brandon Rhodes <brandon@rhodesmill.org>
2013-09-18 20:33:45 -0400

    Modernize 1040 Schedule B

--- luca/forms/us/f1040sb.py
+++ luca/forms/us/f1040sb.py
@@ -1,10 +1,11 @@
-from luca.kit import Decimal, cents
+from luca.kit import Decimal, cents, zzstr
 
 title = u'Schedule B (Form 1040): Interest and Ordinary Dividends'
 zero = cents(0)
 
 def defaults(form):
     f = form
+    f.form_version = '2012'
     f.name = ''
     f.ssn = ''
 
@@ -22,59 +23,40 @@ def compute(form):
     f.line4 = f.line2 - f.line3
     f.line6 = sum((amount for payer, amount in f.line5), zero)
 
-def fill(form, fields):
+def fill_out(form, pdf):
     f = form
+    pdf.load('us.f1040sb--{}.pdf'.format(f.form_version))
 
-    def put(n, value):
-        sa, sb = zz(value)
-        fields['%02d[' % (n+0)] = sa
-        fields['%02d[' % (n+1)] = sb
+    pdf['_01['] = f.name
+    pdf['_02['] = f.ssn
 
-    fields['_01['] = f.name
-    fields['_02['] = f.ssn
+    pdf.pattern = 'f1-{:02}['
 
     n = 3
     for payer, amount in f.line1:
-        fields['-%02d[' % n] = payer
-        put(n+1, amount)
+        pdf[n] = payer
+        pdf[n+1], pdf[n+2] = zzstr(amount)
         n += 3
 
-    put(45, f.line2)
-    put(47, f.line3)
-    put(49, f.line4)
+    pdf[45], pdf[46] = zzstr(f.line2)
+    pdf[47], pdf[48] = zzstr(f.line3)
+    pdf[49], pdf[50] = zzstr(f.line4)
 
     n = 51
     for payer, amount in f.line5:
-        fields['%02d[' % n] = payer
-        put(n+1, amount)
+        pdf[n] = payer
+        pdf[n+1], pdf[n+2] = zzstr(amount)
         n += 3
 
-    put(96, f.line6)
+    pdf[96], pdf[97] = zzstr(f.line6)
+    pdf[98] = f.line7b
+
+    pdf.pattern = '{}'
+
     a, b = f.line7a
-    fields['-cb1[0]'] = 'yes' if a else 'Off'
-    fields['-cb1[1]'] = 'no' if not a else 'Off'
-    fields['-cb2[0]'] = 'yes' if b else 'Off'
-    fields['-cb2[1]'] = 'no' if not b else 'Off'
-    fields['98['] = f.line7b
-    fields['-cb3[0]'] = 'yes' if f.line8 else 'Off'
-    fields['-cb3[1]'] = 'no' if not f.line8 else 'Off'
-
-# General-purpose functions that will probably be factored out of here:
-
-def yesno(value, fields):
-    if value:
-        fields[0] = 'Yes'
-    else:
-        fields[1] = 'No'
-
-def z(value):
-    if not value:
-        return u''
-    return unicode(value)
-
-def zz(value):
-    if not isinstance(value, Decimal):
-        return value
-    if not value:
-        return (u'', u'')
-    return unicode(value).split('.')
+    pdf['-cb1[0]'] = 'yes' if a else 'Off'
+    pdf['-cb1[1]'] = 'no' if not a else 'Off'
+    pdf['-cb2[0]'] = 'yes' if b else 'Off'
+    pdf['-cb2[1]'] = 'no' if not b else 'Off'
+    pdf['-cb3[0]'] = 'yes' if f.line8 else 'Off'
+    pdf['-cb3[1]'] = 'no' if not f.line8 else 'Off'
