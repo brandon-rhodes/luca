211628fc51e2cbd0b1703707b7c5fb43b845c0e5
Brandon Rhodes <brandon@rhodesmill.org>
2013-11-01 19:55:59 -0400

    Try categorizing some transactions with YAML rules

--- luca/trial.py
+++ luca/trial.py
@@ -1,9 +1,23 @@
+# -*- coding: utf-8 -*-
 """Trial of rule-driven accounting."""
 
 import re
 import subprocess
-from bottle import route, run, template
+from datetime import date
 from decimal import Decimal
+
+import yaml
+from StringIO import StringIO
+from bottle import route, run, template
+
+sample_yaml = u"""\
+- 2013-09:
+  - / MA$/: Expenses.Travel.boxborough9
+  - / NH$/: Expenses.Travel.boxborough9
+"""
+print yaml.safe_load(StringIO(sample_yaml))
+# import sys
+# sys.exit(0)
 
 transaction_start = re.compile(ur"""
     \s*(\d*)          # "Reference Number"
@@ -15,6 +29,33 @@ transaction_start = re.compile(ur"""
 
 class T(object):
     pass
+
+def process_transactions(transactions, rule):
+    if isinstance(rule, str) or isinstance(rule, unicode):
+        category = unicode(rule)
+        for t in transactions:
+            t.category = category
+    elif isinstance(rule, list):
+        for item in rule:
+            process_transactions(transactions, item)
+    elif isinstance(rule, dict):
+        for key, value in rule.items():
+            if key.startswith('/') and key.endswith('/'):
+                r = re.compile(key[1:-1])
+                process_transactions(
+                    [t for t in transactions
+                     if r.search(t.description)
+                     or any(r.search(c) for c in t.comments)],
+                    value,
+                    )
+            else: # 'YYYY-MM'
+                year = int(key[:4])
+                month = int(key[5:7])
+                process_transactions(
+                    [t for t in transactions
+                     if t.date.year == year and t.date.month == month],
+                    value,
+                    )
 
 @route('/')
 def index(name='World'):
@@ -29,13 +70,16 @@ def index(name='World'):
         match = transaction_start.match(line)
         if match:
             t = T()
-            t.date = match.group(2)
+            month = int(match.group(2))
+            day = int(match.group(3))
+            t.date = date(2013, month, day)
             description = match.group(6).strip()
             if description.endswith(' **'):
                 description = description[:-3].strip()
             t.description = description
             t.amount = Decimal(match.group(7))
             t.comments = []
+            t.category = None
             transactions.append(t)
             i = match.start(6)
         elif i and line[:i].isspace() and not line[i:i+1].isspace():
@@ -43,9 +87,14 @@ def index(name='World'):
         elif i:
             i = 0
 
+    y = yaml.safe_load(StringIO(sample_yaml))
+    print y
+    process_transactions(transactions, y)
+
     foo = ''
     for t in transactions:
-        foo += '%s %s %s %s\n' % (t.date, t.amount, repr(t.description), t.comments)
+        foo += '%s %s %s %s : %s\n' % (
+            t.date, t.amount, repr(t.description), t.comments, t.category)
 
     return template('<pre>{{foo}}</pre><hr><pre>{{name}}</pre>!',
                     foo=foo, name=content)
