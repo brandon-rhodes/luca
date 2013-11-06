20bed1d56fc94a3486e46a0338f33017b46c8c1a
Brandon Rhodes <brandon@rhodesmill.org>
2013-11-05 21:42:49 -0500

    Tweak code to make January less of a special case

--- luca/importer/dccu.py
+++ luca/importer/dccu.py
@@ -86,15 +86,15 @@ def import_dccu_visa_pdf(text, Transaction):
         stripped_line = line.lstrip()
         if stripped_line.startswith('Statement Closing Date'):
             closing = stripped_line.split()[3].split('/')  # '01/10/2013'
-            year = int(closing[2])
-            is_january = int(closing[0]) == 1
+            closing_year = int(closing[2])
+            closing_month = int(closing[0])
         match = _visa_transaction_re.match(line)
         if match:
             t = Transaction()
             month = int(match.group(2))
             day = int(match.group(3))
-            bump = -1 if is_january and month > 1 else 0
-            t.date = date(year + bump, month, day)
+            year = closing_year - 1 if month > closing_month else closing_year
+            t.date = date(year, month, day)
             description = match.group(6).strip()
             if description.endswith(' **'):
                 description = description[:-3].strip()
