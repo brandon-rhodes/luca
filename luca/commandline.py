c55e1d12a495a3eb642dcd5e6d4b9658eda23bbc
Brandon Rhodes <brandon@rhodesmill.org>
2013-11-10 13:15:24 -0600

    Remove old command-line subcommands

--- luca/commandline.py
+++ luca/commandline.py
@@ -13,8 +13,6 @@ Options:
   -v    Verbose: display the transactions that make up each category
 
 """
-import os
-import sys
 import textwrap
 
 import blessings
@@ -23,9 +21,7 @@ from docopt import docopt
 import luca.forms.actions
 import luca.importer.dccu
 import luca.importer.yodlee
-from operator import attrgetter
 from . import files
-from .model import Transaction
 from .ofx import io
 
 def main():
@@ -80,98 +76,3 @@ def old_download_command(args):
     for op in operations:
         data = io.download_activity(login.fi, login.username, login.password, op)
         files.ofx_create(nickname + '-activity-DATE.xml', data)
-
-def old_merge_command(args):
-    logins = files.read_logins()
-    transactions = []
-    for (nickname, login) in sorted(logins.items()):
-        #accounts = files.get_most_recent_accounts(login)
-        balances, more_transactions = files.get_most_recent_activity(login)
-        for tranlist in more_transactions.values():
-            transactions.extend(tranlist)
-    transactions.sort(key=attrgetter('dtposted'))
-    for t in transactions:
-        # print t.fitid
-        date = '/'.join((t.dtposted[0:4], t.dtposted[4:6], t.dtposted[6:8]))
-        p0, p1 = '()' if t.trnamt < 0 else '  '
-
-        print 'a', t.trntype, t.trnamt
-        print 'b', '   {} {!r}'.format(
-            p0 + str(abs(t.trnamt)) + p1, t.name,
-            )
-
-        print date,
-        if hasattr(t, 'checknum'):
-            print '({})'.format(t.checknum.strip('0'))
-        print
-        print
-
-
-def old_import_subcommand(args):
-    transactions = []
-    for path in args.path:
-        more = import_action(path)
-        transactions.extend(more)
-
-    transactions.sort(key=Transaction.key)
-    for t in transactions:
-        t.format_for_ledger()
-
-def import_action(path):
-    if not os.path.exists(path):
-        print >>sys.stderr, 'No such path:', path
-        exit(1)
-
-    return luca.importer.dccu.parse(path)
-
-    with open(path) as f:
-        content = f.read()
-    assert luca.importer.yodlee.matches(content)
-    luca.importer.yodlee.parse(content)
-
-
-def status(args):
-    logins = files.read_logins()
-    emptylist = []
-    for (nickname, login) in sorted(logins.items()):
-        print nickname, '-',
-        accounts = files.get_most_recent_accounts(login)
-        balances, transactions = files.get_most_recent_activity(login)
-        if accounts is None:
-            print
-            print '  you have never run "luca download -a {}"'.format(nickname)
-        elif balances is None:
-            print
-            print '  you have never run "luca download {}"'.format(nickname)
-        else:
-            print
-        if accounts:
-            for account in accounts:
-                print '  {:20} {:14}'.format(
-                    account.acctid,
-                    getattr(account, 'accttype', ''),
-                    ),
-                if balances:
-                    if account.key in balances:
-                        balance = balances[account.key]
-                        print '{:>12}'.format(negparen(balance)),
-                    else:
-                        print '(no balance information)',
-                tlist = transactions.get(account.key, emptylist)
-                print
-                if tlist:
-                    for t in tlist:
-                        print '{}-{}-{}'.format(t.dtposted[0:4],
-                                                t.dtposted[4:6],
-                                                t.dtposted[6:8]),
-                        print repr(t.name),
-                        print t.trnamt
-                if tlist:
-                    print '{:>5} new transactions'.format(len(tlist))
-                else:
-                    print '      up-to-date'
-
-def negparen(amount):
-    if amount < 0:
-        return '({:.2f})'.format(-amount)
-    return '{:.2f} '.format(amount)
