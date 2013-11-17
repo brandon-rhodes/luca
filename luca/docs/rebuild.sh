a011324c878148e41eef689d67039daa96b1c460
Brandon Rhodes <brandon@rhodesmill.org>
2013-11-17 16:38:36 -0500

    Build fix: output4 should simply be categories

--- luca/docs/rebuild.sh
+++ luca/docs/rebuild.sh
@@ -5,7 +5,13 @@ STATEMENTS="statement-bank.txt statement-visa.txt"
 for rulefile in rules*.yaml
 do
     base=$(echo $rulefile | sed 's/rules/output/;s/.yaml//')
-    TERM=ansi COLUMNS=48 luca tally -ct $rulefile $STATEMENTS > $base.txt
+    if echo $rulefile | grep -q 't\.'
+    then
+        opt="-ct"
+    else
+        opt="-c"
+    fi
+    TERM=ansi COLUMNS=48 luca tally $opt $rulefile $STATEMENTS > $base.txt
     (echo '<pre>';
         cat $base.txt | ansi2html -i;
         echo '</pre>') > $base.html
