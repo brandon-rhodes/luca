900d764952ffa8035d00817d39faa6e54ba2f78f
Brandon Rhodes <brandon@rhodesmill.org>
2013-11-18 14:13:13 -0500

    Final metadata update for 1.0

--- setup.py
+++ setup.py
@@ -1,20 +1,25 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 import luca
 
 description, long_description = luca.__doc__.split('\n', 1)
 
 setup(name='luca',
-      version='0.1',
+      version='1.0',
       description=description.strip(),
       long_description=long_description.strip(),
       author='Brandon Rhodes',
       author_email='brandon@rhodesmill.org',
       url='https://github.com/brandon-rhodes/luca',
       classifiers=[
-        'Development Status :: 1 - Alpha',
-        'License :: OSI Approved :: BSD License',
-        ],
-      packages=['luca'],
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Console',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Office/Business',
+          'Topic :: Office/Business :: Financial',
+          'Topic :: Office/Business :: Financial :: Accounting',
+          ],
+      packages=find_packages(),
       install_requires=[
         'blessings',
         'docopt',
