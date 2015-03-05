81d491fd91beaf642a2e3685ad81504b2ea43e18
Brandon Rhodes <brandon@rhodesmill.org>
2015-03-05 13:16:42 -0500

    Fix: parser ignored all CSV rows; move try/except

--- luca/importer/autocsv.py
+++ luca/importer/autocsv.py
@@ -24,46 +24,45 @@ def importer(csvfile):
     transactions = []
 
     for row in reader:
-        _parse(row, balances, transactions)
+        try:
+            _parse(row, balances, transactions)
+        except ValueError as e:
+            print('{0}: ignoring CSV line because luca {1}:\n{2}\n'.format(
+                csvfile.name, e, row, file=sys.stderr))
 
     return balances, transactions
 
 def _parse(row, balances, transactions):
-    try:
-        # TODO: better way to detect header line
-        if row[0] == 'Type':
-            return
+    # TODO: better way to detect header line
+    # if row[0] == 'Type':
+    #     return
 
-        # print row
-        date = amount = None
-        description = []
+    # print row
+    date = amount = None
+    description = []
 
-        for field in row:
-            m = date_match(field)
-            if m:
-                date = datetime.strptime(field, '%m/%d/%Y').date()
-                return
-            a = amount_match(field)
-            if a:
-                amount = Decimal(a.group(1) + a.group(2).replace(',', ''))
-                return
-            field = field.strip()
-            if field:
-                description.append(field)
+    for field in row:
+        m = date_match(field)
+        if m:
+            date = datetime.strptime(field, '%m/%d/%Y').date()
+            continue
+        a = amount_match(field)
+        if a:
+            amount = Decimal(a.group(1) + a.group(2).replace(',', ''))
+            continue
+        field = field.strip()
+        if field:
+            description.append(field)
 
-        if date is None:
-            raise ValueError('cannot find date field')
-        if amount is None:
-            raise ValueError('cannot find amount field')
+    if date is None:
+        raise ValueError('cannot find date field')
+    if amount is None:
+        raise ValueError('cannot find amount field')
 
-        t = Transaction()
-        t.account = 'Checking'
-        t.date = date
-        t.description = ' '.join(description)
-        t.amount = amount
+    t = Transaction()
+    t.account = 'Checking'
+    t.date = date
+    t.description = ' '.join(description)
+    t.amount = amount
 
-        transactions.append(t)
-
-    except ValueError as e:
-        print('Ignoring CSV line because luca {0}:\n{1}\n'.format(
-            e, row, file=sys.stderr))
+    transactions.append(t)
