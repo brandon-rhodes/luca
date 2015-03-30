b406bda7238081ee4dbe18a84ad98f694deb5e6f
Brandon Rhodes <brandon@rhodesmill.org>
2015-03-30 03:30:12 -0400

    Get form 1040 basically working for 2014

--- luca/forms/us/f1040.py
+++ luca/forms/us/f1040.py
@@ -8,7 +8,7 @@ filing_statuses = 'S MJ MS HoH QW'.split()
 exemptions = {
     u'2012': Decimal('3800.00'),
     u'2013': Decimal('3900.00'),
-    u'2014': Decimal('3900.00'),
+    u'2014': Decimal('3950.00'),
     }
 
 def lines(seq):
@@ -20,12 +20,23 @@ income_lines = lines('7 8a 8b 9a 9b 10 11 12 13 14 15a 15b '
                      '16a 16b 17 18 19 20a 20b 21')
 income_tally = lines('7 8a 9a 10 11 12 13 14 15b 16b 17 18 19 20b 21')
 adjustments = lines('23 24 25 26 27 28 29 30 31a 32 33 34 35')
-credits = lines(range(47, 54))
-other_taxes = lines('56 57 58 59a 59b 60')
-payments = lines(range(62, 64) + ['64a', '64b'] + range(65, 72))
+
+class info2013:
+    credits = lines(range(47, 54))
+    other_taxes = lines('56 57 58 59a 59b 60')
+    payments = lines(range(62, 64) + ['64a', '64b'] + range(65, 72))
+
+class info2014:
+    credits = lines(range(48, 55))
+    other_taxes = lines('57 58 59 60a 60b 61 62')
+    payments = lines([64, 65, '66a', '66b'] + range(67, 74))
+
+info_dict = {u'2012': info2013, u'2013': info2013, u'2014': info2014}
 
 def defaults(form):
     f = form
+    v = info_dict[f.form_version]
+
     f.ssn = u''
     f.spouse_ssn = u''
 
@@ -45,7 +56,8 @@ def defaults(form):
     f.line6a = False
     f.line6b = False
 
-    for line in income_lines + adjustments + credits + other_taxes + payments:
+    for line in (income_lines + adjustments + v.credits
+                 + v.other_taxes + v.payments):
         setattr(f, line, zero)
 
     f.line21_text = u''
@@ -54,6 +66,9 @@ def defaults(form):
     f.line40 = zero
     f.line45 = zero
 
+    f.line76a = zero
+    f.line77 = zero
+    f.line79 = zero
 
 def check(form, forms, eq):
     nothing = [None]
@@ -101,6 +116,8 @@ def check(form, forms, eq):
 
 def compute(form):
     f = form
+    v = info_dict[f.form_version]
+
     f.line6ab = bool(f.line6a) + bool(f.line6b)
     # TODO: dependents
     f.line6d = f.line6ab
@@ -118,17 +135,35 @@ def compute(form):
     else:
         schedule = schedules[form.form_version][f.filing_status]
         f.line44 = schedule.compute_tax_on(f.line43)
+
+    if f.form_version < u'2014':
+        return _compute_pre_2014(f, v)
+
+    f.line46 = zero
+    f.line47 = f.line44 + f.line45 + f.line46
+    f.line55 = dsum(getattr(f, line) for line in v.credits)
+    f.line56 = f.line47 - f.line55
+    f.line63 = f.line56 + dsum(getattr(f, line) for line in v.other_taxes)
+    f.line74 = dsum(getattr(f, line) for line in v.payments) - f.line66b
+    if f.line74 > f.line63:
+        f.line75 = f.line74 - f.line63
+        f.line78 = zero
+        # TODO: asking for refund etc
+    elif f.line63 > f.line74:
+        f.line75 = zero
+        f.line78 = f.line63 - f.line74
+
+def _compute_pre_2014(f, v):
     f.line46 = f.line44 + f.line45
-    f.line54 = dsum(getattr(f, line) for line in credits)
+    f.line54 = dsum(getattr(f, line) for line in v.credits)
     f.line55 = f.line46 - f.line54
-    f.line61 = f.line55 + dsum(getattr(f, line) for line in other_taxes)
-    f.line72 = dsum(getattr(f, line) for line in payments) - f.line64b
+    f.line61 = f.line55 + dsum(getattr(f, line) for line in v.other_taxes)
+    f.line72 = dsum(getattr(f, line) for line in v.payments) - f.line64b
     if f.line72 > f.line61:
         f.line73 = f.line72 - f.line61
         # TODO: asking for refund etc
     elif f.line61 > f.line72:
         f.line76 = f.line61 - f.line72
-
 
 def fill_out(form, pdf):
     pdf.load('us.f1040--{}.pdf'.format(form.form_version))
@@ -137,6 +172,8 @@ def fill_out(form, pdf):
         return fill_out_2013_and_before(form, pdf)
 
     f = form
+    #v = info_dict[f.form_version]
+
     pdf.pattern = 'f1_{:02}[0]'
 
     pdf[4] = f.first_name
@@ -163,7 +200,8 @@ def fill_out(form, pdf):
     pdf['Page1[0].c1_03[0]'] = ('HoH' if f.filing_status == 'HoH' else 'Off')
     pdf['Page1[0].c1_03[1]'] = ('QW' if f.filing_status == 'QW' else 'Off')
 
-    pdf['Lines1-3[0].f1-16[0]'] = f.spouse_ssn
+    if f.filing_status == 'MS':
+        pdf['Lines1-3[0].f1-16[0]'] = f.spouse_ssn
 
     pdf['c1_04[0]'] = ('1' if f.line6a else 'Off')
     pdf['c1_05[0]'] = ('1' if f.line6b else 'Off')
@@ -176,12 +214,36 @@ def fill_out(form, pdf):
     pdf[34] = f.line6d
 
     i = 35
-    for n in '7 8a 8b 9a 9b'.split():
+    for n in ('7 8a 8b 9a 9b 10 11 12 13 14 15a 15b 16a 16b 17 18 19 '
+              '20a 20b 21 22 23 24 25 26 27 28 29 '
+              '30 31a 32 33 34 35 36 37').split():
         pdf[i], pdf[i+1] = zzstr(f['line', n])
-        i += 2
+        if n in ('21', '31a'):
+            i += 3
+        else:
+            i += 2
+
+    pdf.pattern = '{}'
+    pdf['f1-_51[0]'] = zzstr(f.line13)[0]
+
+    pdf.pattern = '.f2_{:02}[0]'
+
+    i = 1
+    for n in ([38] + range(40, 60) + '60a 60b'.split() + range(61, 66)
+              + '66a 66b'.split() + range(67, 76) + '76a 77 78 79'.split()):
+        print i, n, f['line', n]
+        pdf[i], pdf[i+1] = zzstr(f['line', n])
+        if n == '76a':
+            i += 4
+        elif n in (38, 43, 53, 61, 72):
+            i += 3
+        else:
+            i += 2
 
 def fill_out_2013_and_before(form, pdf):
     f = form
+    v = info_dict[f.form_version]
+
     pdf.pattern = 'p1-t{}['
 
     pdf[4] = f.first_name
@@ -235,8 +297,8 @@ def fill_out_2013_and_before(form, pdf):
     pdf.pattern = 'p2-t{}[0]'
 
     n = 1
-    for line in (lines([38] + range(40, 47)) + credits + lines('54 55')
-                 + other_taxes + lines('61') + payments + lines('72 73 74a')
+    for line in (lines([38] + range(40, 47)) + v.credits + lines('54 55')
+                 + v.other_taxes + lines('61') + v.payments + lines('72 73 74a')
                  + lines(range(75, 78))):
         pdf[n], pdf[n+1] = zzstr(getattr(f, line, zero)) # todo: remove default
         if f.form_version == u'2013':
@@ -290,7 +352,7 @@ def tax_from_tax_table(taxable_income, filing_status):
 
 schedules = {
     u'2013': {
-        u'S': TaxSchedule(one_allowance=3900, brackets = [
+        u'S': TaxSchedule([
             (0, 10),
             (8925, 15),
             (36250, 25),
@@ -299,7 +361,7 @@ schedules = {
             (398350, 35),
             (400000, '39.6'),
         ]),
-        u'MJ': TaxSchedule(one_allowance=3900, brackets = [
+        u'MJ': TaxSchedule([
             (0, 10),
             (17850, 15),
             (72500, 25),
@@ -308,7 +370,7 @@ schedules = {
             (398350, 35),
             (450000, '39.6'),
         ]),
-        u'MS': TaxSchedule(one_allowance=3900, brackets = [
+        u'MS': TaxSchedule([
             (0, 10),
             (8925, 15),
             (36250, 25),
@@ -317,7 +379,7 @@ schedules = {
             (199175, 35),
             (225000, '39.6'),
         ]),
-        u'HH': TaxSchedule(one_allowance=3900, brackets = [
+        u'HH': TaxSchedule([
             (10, 0),
             (12750, 15),
             (48600, 25),
@@ -328,7 +390,7 @@ schedules = {
         ]),
     },
     u'2014': {
-        u'S': TaxSchedule(one_allowance=3900, brackets = [
+        u'S': TaxSchedule([
             (0, 10),
             (9075, 15),
             (36900, 25),
@@ -337,7 +399,7 @@ schedules = {
             (405100, 35),
             (406750, '39.6'),
         ]),
-        u'MJ': TaxSchedule(one_allowance=3900, brackets = [
+        u'MJ': TaxSchedule([
             (0, 10),
             (18150, 15),
             (73800, 25),
@@ -346,7 +408,7 @@ schedules = {
             (405100, 35),
             (457600, '39.6'),
         ]),
-        u'MS': TaxSchedule(one_allowance=3900, brackets = [
+        u'MS': TaxSchedule([
             (0, 10),
             (9075, 15),
             (36900, 25),
@@ -355,7 +417,7 @@ schedules = {
             (202550, 35),
             (228800, '39.6'),
         ]),
-        u'HH': TaxSchedule(one_allowance=3900, brackets = [
+        u'HH': TaxSchedule([
             (0, 10),
             (12950, 15),
             (49400, 25),
