ed78b268366d20d9de974405682b5caf2b26a566
Brandon Rhodes <brandon@rhodesmill.org>
2017-04-29 10:45:00 -0400

    Update Form 941 for 2017

--- luca/forms/us/f941.py
+++ luca/forms/us/f941.py
@@ -28,8 +28,15 @@ def defaults(form):
     f.line7 = zero
     f.line8 = zero
     f.line9 = zero
-    f.line11 = None  # in which case we compute it from other information
-    if f.form_version < u'2014':
+    if f.form_version == u'2017':
+        f.line11 = zero
+        f.line13 = zero
+        f.line16 = 'a'
+        f.line16_month1 = zero
+        f.line16_month2 = zero
+        f.line16_month3 = zero
+    elif f.form_version < u'2014':
+        f.line11 = None  # in which case we compute it from other information
         f.line12a = zero
         f.line12b = 0
         f.line16 = 'a'
@@ -37,7 +44,8 @@ def defaults(form):
         f.line16_month2 = zero
         f.line16_month3 = zero
     else:
-        f.line14 = 'a'
+        f.line11 = None  # in which case we compute it from other information
+        # f.line14 = 'a'
         f.line14_month1 = zero
         f.line14_month2 = zero
         f.line14_month3 = zero
@@ -70,7 +78,16 @@ def compute(form):
         f.line5d = f.line5a2 + f.line5b2 + f.line5c2
         f.line6 = f.line3 + f.line5d + getattr(f, 'line5e', zero)
     f.line10 = f.line6 + f.line7 + f.line8 + f.line9
-    if f.form_version >= u'2014':
+    if f.form_version == u'2017':
+        f.line12 = f.line10 - f.line11
+        f.line16_total = f.line16_month1 + f.line16_month2 + f.line16_month3
+        if f.line12 > f.line13:
+            f.line14 = f.line10 - f.line13
+            f.line15 = zero
+        else:
+            f.line14 = zero
+            f.line15 = f.line13 - f.line10
+    elif f.form_version >= u'2014':
         f.line14_total = f.line14_month1 + f.line14_month2 + f.line14_month3
         if f.line11 is None:
             f.line11 = f.line14_total
@@ -100,9 +117,92 @@ def fill_out(form, pdf):
     pdf.load('us.f941--{}.pdf'.format(f.year))
     pdf.pages = 1, 2
 
+    if f.form_version <= u'2016':
+        fill_out_2016_and_earlier(form, pdf)
+        return
+
     if f.form_version <= u'2013':
         fill_out_2013_and_earlier(form, pdf)
         return
+
+    pdf.pattern = '.c1_01[{}]'
+
+    pdf[0] = '1' if f.quarter == 1 else 'Off'
+    pdf[1] = '2' if f.quarter == 2 else 'Off'
+    pdf[2] = '3' if f.quarter == 3 else 'Off'
+    pdf[3] = '4' if f.quarter == 4 else 'Off'
+
+    pdf.pattern = '.f1_{:02}[0]'
+
+    def pair(n, value, show_zero=False):
+        pdf[n], pdf[n + 1] = nnstr(value) if show_zero else zzstr(value)
+
+    for i in range(9):
+        pdf[i + 1] = f.ein.replace('-', '')[i : i+1]
+
+    pdf[10] = f.name
+    pdf[11] = f.trade_name
+    pdf[12] = f.address
+    pdf[13] = f.city
+    pdf[14] = f.state
+    pdf[15] = f.zip
+
+    pdf[19] = str(f.line1)
+
+    pair(20, f.line2, True)
+    pair(22, f.line3, True)
+
+    pair(24, f.line5a1)
+    pair(26, f.line5a2)
+    pair(28, f.line5b1)
+    pair(30, f.line5b2)
+    pair(32, f.line5c1)
+    pair(34, f.line5c2)
+    pair(36, f.line5d1)
+    pair(38, f.line5d2)
+    pair(40, f.line5e, True)
+    pair(42, f.line5f)
+    pair(44, f.line6, True)
+    pair(46, f.line7)
+    pair(48, f.line8)
+    pair(50, f.line9)
+    pair(52, f.line10, True)
+    pair(54, f.line11)
+    pair(56, f.line12, True)
+    pair(58, f.line13)
+    pair(60, f.line14)
+    pair(62, f.line15)
+
+    pdf.pattern = '{}'
+
+    pdf['.c2_01[0]'] = '1' if f.line16 == 'a' else 'Off'
+    pdf['.c2_01[1]'] = '2' if f.line16 == 'b' else 'Off'
+    pdf['.c2_01[2]'] = '3' if f.line16 == 'c' else 'Off'
+
+    pdf.pattern = '.f2_{:02}[0]'
+
+    pdf[1] = f.name
+    pdf[2] = f.ein
+
+    pair(3, f.line16_month1, True)
+    pair(5, f.line16_month2, True)
+    pair(7, f.line16_month3, True)
+    pair(9, f.line16_total, True)
+
+    pdf[19] = f.signer_name
+    pdf[20] = f.signer_title
+    pdf[21] = f.signer_phone
+
+    # # TODO: support Part 3
+    # # TODO: allow "yes" and further information for Part 4
+
+    # pdf.pattern = '{}'
+
+    # pdf[] = 'Yes' if f.Part_4 else 'Off'
+    # pdf[] = 'Off' if f.Part_4 else 'No'
+
+def fill_out_2016_and_earlier(form, pdf):
+    f = form
 
     pdf['f_01_0_[0]'] = f.name
     pdf['f2_02_0_[0]'] = f.ein
